51ce25381459d9314a47bbc63d494b5eb8c0fd2b
Campbell Vertesi <campbell@vertesi.com>
2020-09-29 17:18:52 +0200

    Add first function and test

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+
+}
+
+func removeScript(s string) string {
+
+	return s
+}
---
+++ main_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"strings"
+	"testing"
+)
+
+func TestFindScript(t *testing.T) {
+	dirtyHTML, err := ioutil.ReadFile("test/19.html")
+	if err != nil {
+		log.Fatalf("Could not read dirty HTML file. Error: %q", err)
+	}
+	dirtyHTMLString := string(dirtyHTML)
+	cleanHTML := removeScript(dirtyHTMLString)
+	if strings.Contains(cleanHTML, "coinhive") {
+		t.Error("Coinhive text was not removed from the HTML")
+	}
+}
