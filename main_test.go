a68c6ecfd351226fa4441bbd35faca45846291c9
Campbell Vertesi <campbell@vertesi.com>
2020-09-30 10:07:41 +0200

    Revert "Wrap test in t.Run"
    This reverts commit da7837206e8bf71374c7929a59a090c80ae73b4c.

--- main_test.go
+++ main_test.go
@@ -8,15 +8,13 @@ import (
 )
 
 func TestFindScript(t *testing.T) {
-	t.Run("Clean external script include", func(t *testing.T) {
-		dirtyHTML, err := ioutil.ReadFile("test/19.html")
-		if err != nil {
-			log.Fatalf("Could not read dirty HTML file. Error: %q", err)
-		}
-		dirtyHTMLString := string(dirtyHTML)
-		cleanHTML := removeScriptFromHTML(dirtyHTMLString)
-		if strings.Contains(cleanHTML, "coinhive") {
-			t.Error("Coinhive text was not removed from the HTML")
-		}
-	})
+	dirtyHTML, err := ioutil.ReadFile("test/19.html")
+	if err != nil {
+		log.Fatalf("Could not read dirty HTML file. Error: %q", err)
+	}
+	dirtyHTMLString := string(dirtyHTML)
+	cleanHTML := removeScriptFromHTML(dirtyHTMLString)
+	if strings.Contains(cleanHTML, "coinhive") {
+		t.Error("Coinhive text was not removed from the HTML")
+	}
 }
